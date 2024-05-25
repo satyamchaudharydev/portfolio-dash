1cf4e2f5b20fc935ad65c05503e85712140f06d2
satyam_chaudhary <satyam.chaudhary@brightchamps.com>
2024-05-25 11:17:07 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
   redirect("/home");
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       "Interesting"
