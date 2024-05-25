8944a81b77a0a7184329786fea4862fb4a57fe7a
satyam_chaudhary <satyam.chaudhary@brightchamps.com>
2024-05-25 10:44:28 +0530

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Nunito_Sans } from 'next/font/google'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
+  : "http://localhost:3000"
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
-
+const arimo = Nunito_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+    <html lang="en" className={arimo.className}>
+      <body className="bg-secondary-foreground text-foreground  w-full">
           {children}
-        </main>
+        
       </body>
     </html>
   );
